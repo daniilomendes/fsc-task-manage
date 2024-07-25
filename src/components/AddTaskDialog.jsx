ada46e0ca33ccb42a7ecede59cf5e9624decb38c
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-25 10:24:10 -0300

    feat: ensure error is shown if required fields are not filled on AddTaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -25,6 +25,10 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
   }, [isOpen]);
 
   const handleSaveClick = () => {
+    if (!title.trim() || !time.trim() || !description.trim()) {
+      return alert("Preencha todos os campos!");
+    }
+
     handleSubmit({
       id: v4(),
       title,
