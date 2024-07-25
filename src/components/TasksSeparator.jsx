ad1aef66be6ad207ea8aff9dfc530ce9c9754fc7
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-25 19:48:00 -0300

    chore: add prop-types to Button component

--- src/components/TasksSeparator.jsx
+++ src/components/TasksSeparator.jsx
@@ -11,7 +11,7 @@ const TasksSeparator = ({ icon, title }) => {
 
 TasksSeparator.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.element.isRequired,
+  icon: PropTypes.node.isRequired,
 };
 
 export default TasksSeparator;
