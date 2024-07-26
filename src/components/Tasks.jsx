58f4eca6c217cfb1716b99e393a34bfeaaca33e0
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-25 21:02:46 -0300

    feat: ensure task is deleted on db

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -61,7 +61,17 @@ const Tasks = () => {
     setTasks(newTasks);
   };
 
-  const handleTaskDeleteClick = (taskId) => {
+  const handleTaskDeleteClick = async (taskId) => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      return toast.error(
+        "Erro ao deletar a tarefa. Por favor, tente novamente."
+      );
+    }
+
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
 
